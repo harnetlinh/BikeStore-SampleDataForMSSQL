aecb431987e82b488e7fccb4d0ba8794a7654236
Ha Ngoc Linh <49241285+harnetlinh@users.noreply.github.com>
2023-10-04 16:11:21 +0700

    Update BikeStores(01)-TableStructureOnlyWithoutForeignKey.sql

--- BikeStores(01)-TableStructureOnlyWithoutForeignKey.sql
+++ BikeStores(01)-TableStructureOnlyWithoutForeignKey.sql
@@ -3,32 +3,33 @@ go
 USE BikeStores;
 go
 
--- create tables
 CREATE TABLE categories (
-	category_id INT PRIMARY KEY,
-	category_name VARCHAR (255)
+	category_id INT IDENTITY (1, 1) PRIMARY KEY,
+	category_name VARCHAR (255) NOT NULL
 );
 
 CREATE TABLE brands (
-	brand_id INT PRIMARY KEY,
-	brand_name VARCHAR (255)
+	brand_id INT IDENTITY (1, 1) PRIMARY KEY,
+	brand_name VARCHAR (255) NOT NULL
 );
 
 CREATE TABLE products (
-	product_id INT PRIMARY KEY,
-	product_name VARCHAR (255),
-	brand_id INT,
-	category_id INT,
-	model_year SMALLINT,
-	list_price DECIMAL (10, 2)
+	product_id INT IDENTITY (1, 1) PRIMARY KEY,
+	product_name VARCHAR (255) NOT NULL,
+	brand_id INT NOT NULL,
+	category_id INT NOT NULL,
+	model_year SMALLINT NOT NULL,
+	list_price DECIMAL (10, 2) NOT NULL,
+	FOREIGN KEY (category_id) REFERENCES categories (category_id) ,
+	FOREIGN KEY (brand_id) REFERENCES brands (brand_id) 
 );
 
 CREATE TABLE customers (
-	customer_id INT PRIMARY KEY,
-	first_name VARCHAR (255),
-	last_name VARCHAR (255),
+	customer_id INT IDENTITY (1, 1) PRIMARY KEY,
+	first_name VARCHAR (255) NOT NULL,
+	last_name VARCHAR (255) NOT NULL,
 	phone VARCHAR (25),
-	email VARCHAR (255),
+	email VARCHAR (255) NOT NULL,
 	street VARCHAR (255),
 	city VARCHAR (50),
 	state VARCHAR (25),
@@ -36,8 +37,8 @@ CREATE TABLE customers (
 );
 
 CREATE TABLE stores (
-	store_id INT PRIMARY KEY,
-	store_name VARCHAR (255),
+	store_id INT IDENTITY (1, 1) PRIMARY KEY,
+	store_name VARCHAR (255) NOT NULL,
 	phone VARCHAR (25),
 	email VARCHAR (255),
 	street VARCHAR (255),
@@ -48,40 +49,49 @@ CREATE TABLE stores (
 
 CREATE TABLE staffs (
 	staff_id INT IDENTITY (1, 1) PRIMARY KEY,
-	first_name VARCHAR (50),
-	last_name VARCHAR (50),
-	email VARCHAR (255) UNIQUE,
+	first_name VARCHAR (50) NOT NULL,
+	last_name VARCHAR (50) NOT NULL,
+	email VARCHAR (255) NOT NULL UNIQUE,
 	phone VARCHAR (25),
-	active tinyint,
-	store_id INT,
-	manager_id INT
+	active tinyint NOT NULL,
+	store_id INT NOT NULL,
+	manager_id INT,
+	FOREIGN KEY (store_id) REFERENCES stores (store_id) ,
+	FOREIGN KEY (manager_id) REFERENCES staffs (staff_id) 
 );
 
 CREATE TABLE orders (
 	order_id INT IDENTITY (1, 1) PRIMARY KEY,
 	customer_id INT,
-	order_status tinyint,
+	order_status tinyint NOT NULL,
 	-- Order status: 1 = Pending; 2 = Processing; 3 = Rejected; 4 = Completed
-	order_date DATE,
-	required_date DATE,
+	order_date DATE NOT NULL,
+	required_date DATE NOT NULL,
 	shipped_date DATE,
-	store_id INT,
-	staff_id INT
+	store_id INT NOT NULL,
+	staff_id INT NOT NULL,
+	FOREIGN KEY (customer_id) REFERENCES customers (customer_id) ,
+	FOREIGN KEY (store_id) REFERENCES stores (store_id) ,
+	FOREIGN KEY (staff_id) REFERENCES staffs (staff_id) 
 );
 
 CREATE TABLE order_items (
 	order_id INT,
 	item_id INT,
-	product_id INT,
-	quantity INT,
-	list_price DECIMAL (10, 2),
-	discount DECIMAL (4, 2) DEFAULT 0,
-	PRIMARY KEY (order_id, item_id)
+	product_id INT NOT NULL,
+	quantity INT NOT NULL,
+	list_price DECIMAL (10, 2) NOT NULL,
+	discount DECIMAL (4, 2) NOT NULL DEFAULT 0,
+	PRIMARY KEY (order_id, item_id),
+	FOREIGN KEY (order_id) REFERENCES orders (order_id) ,
+	FOREIGN KEY (product_id) REFERENCES products (product_id) 
 );
 
 CREATE TABLE stocks (
 	store_id INT,
 	product_id INT,
 	quantity INT,
-	PRIMARY KEY (store_id, product_id)
+	PRIMARY KEY (store_id, product_id),
+	FOREIGN KEY (store_id) REFERENCES stores (store_id) ,
+	FOREIGN KEY (product_id) REFERENCES products (product_id) 
 );
